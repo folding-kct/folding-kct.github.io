f9d99e73825a75a5a4bf115531d6d583be18af83
Aswath S <aswath7862001@gmail.com>
2020-03-30 01:11:23 +0530

    Update team-stats.js

--- assets/team-stats.js
+++ assets/team-stats.js
@@ -2,51 +2,79 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const Mapping = {
         "Date of last work unit": "last",
         "Active CPUs within 50 days": "active_50",
-        "Team Id": "team",
-        "Grand Score": "credit",
+        "Team Id": "id",
+        "Grand Score": "score",
         "Work Unit Count": "wus",
         "Team Ranking": "rank"
     };
 
     var content_from_api
-    const uri = `https://stats.foldingathome.org/api/team/252736`;
+    const uri = `https://api.foldingathome.org/team/252736`;
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    fetch(proxyurl+uri)
+
+
+    fetch(proxyurl + uri)
         .then(res => {
-        return res.json()
-    }).then(response => {
-        content_from_api = response
-        console.log(response);
-        const table = document.getElementById("teaminfobody");
-        for (var key in Mapping) {
-            table_row = document.createElement("tr");
-            const table_key = document.createElement("td");
-            const table_value = document.createElement("td");
-            table_key.style = "font-weight:600";
-            table_key.textContent = key;
-            console.log(content_from_api[Mapping[key]]);
-            table_value.textContent = content_from_api[Mapping[key]]
-            table_row.append(table_key, table_value);
-            table.append(table_row);
-        }
+            return res.json()
+        }).then(response => {
+            content_from_api = response
+            console.log(response);
+            const table = document.getElementById("teaminfobody");
+            for (var key in Mapping) {
+                table_row = document.createElement("tr");
+                const table_key = document.createElement("td");
+                const table_value = document.createElement("td");
+                table_key.style = "font-weight:600";
+                table_key.textContent = key;
+                console.log(content_from_api[Mapping[key]]);
+                if (Mapping[key] == "rank") {
+                    table_value.textContent = content_from_api[Mapping[key]]
+                    table_value.textContent += `/${response.teams}`
+                }
+                else {
+
+                    table_value.textContent = content_from_api[Mapping[key]]
+                }
+                table_row.append(table_key, table_value);
+                table.append(table_row);
+            }
+
+            for (var i of document.getElementsByClassName("loader1")) {
+                i.style = "display:none;";
+            }
+
+        }).catch(err => {
+            console.log(err, "Error While Fetching stats. Come again Later.")
+        })
+
+    const table_2 = document.getElementById("tbody2");
+    const headers = ["Rank", "Name", "Credits", "Work Units"];
+    const tr = document.createElement("tr");
+
+    for (var i of headers) {
+        var something = document.createElement("td");
+        something.style = "font-weight:600;align:center";
+        something.textContent = i;
+        tr.append(something)
+    }
+
+    table_2.append(tr);
 
 
-        const table_2 = document.getElementById("tbody2");
-        const headers = ["Rank","Name","Credits","Work Units"];
-        const tr = document.createElement("tr");
-        
-        for(var i of headers){
-            var something = document.createElement("td");
-            something.style = "font-weight:600;align:center";
-            something.textContent = i;
-            tr.append(something)
+    var donors = [];
+    const proxyuri = "https://cors-anywhere.herokuapp.com/"
+
+    fetch(proxyuri + uri + '/members').then(res => {
+        return res.text();
+    }).then(resp => {
+        var x = JSON.parse(resp)
+        x.shift();
+        for (var i of x) {
+            donors.push({ name: i[0], rank: i[2], credit: i[3], wus: i[4] })
         }
-        
-        table_2.append(tr);
-        
-        for (var donor of content_from_api.donors) {
+        for (var donor of donors) {
             var { name, credit, wus, rank } = donor;
-            if(rank==undefined){rank="N/A"}
+            if (rank == undefined) { rank = "N/A" }
             var obj = { rank, name, credit, wus };
             console.log("Individual people data down |v");
             console.log(obj);
@@ -58,11 +86,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
             }
             table_2.append(tr);
         }
-        for(var i of document.getElementsByClassName("loader")){
-        i.style="display:none;";
+        for (var i of document.getElementsByClassName("loader2")) {
+            i.style = "display:none;";
         }
     }).catch(err => {
-        console.log(err,"Error While Fetching stats. Come again Later.")
+        console.log(err)
     })
 
+
+
 });
+
+
+
